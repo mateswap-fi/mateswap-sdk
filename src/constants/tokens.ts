0ad7e8d4f0d7127d906e9ca59c9f2dfc2ab52ad1
Matienzo <atomarmatienzo@gmail.com>
2024-04-04 16:52:01 +0200

    contracts

--- src/constants/tokens.ts
+++ src/constants/tokens.ts
@@ -12,6 +12,8 @@ export const UXD: TokenMap = {
   [ChainId.LACHAIN_TESTNET]: new Token(ChainId.LACHAIN_TESTNET, UXD_ADDRESS[ChainId.LACHAIN_TESTNET], 18, 'UXD', 'UXD token')
 }
 
+// WLAC
+// https://explorer.lachain.network/address/0x2911a1AB18546cb501628Be8625C7503a2A7DB54
 export const WLAC: TokenMap = {
   [ChainId.LACHAIN]: new Token(ChainId.LACHAIN, WLAC_ADDRESS[ChainId.LACHAIN], 18, 'WLAC', 'Wrapped LAC'),
   [ChainId.LACHAIN_TESTNET]: new Token(ChainId.LACHAIN_TESTNET, WLAC_ADDRESS[ChainId.LACHAIN_TESTNET], 18, 'WLAC', 'Wrapped LAC')
